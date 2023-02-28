85b65a2c9cf528e5429444cda1a5a11b3749f7fe
Pedro Nunes <pedro.icn88@gmail.com>
2023-02-28 11:03:44 -0300

    feat: create TDD test for route /team/:id

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -13,9 +13,7 @@ chai.use(chaiHttp);
 const { expect } = chai;
 
 describe('Testing route /teams', () => {
-  /**
-   * Exemplo do uso de stubs com tipos
-   */
+
   const mockTeams = [
     {
       "id": 1,
@@ -33,23 +31,60 @@ describe('Testing route /teams', () => {
 
   let chaiHttpResponse: Response;
 
-  before(async () => {
+  beforeEach(async () => {
     sinon
       .stub(TeamModel, "findAll")
       .resolves(mockTeams as TeamModel[]);
   });
 
-  after(() => {
+  afterEach(() => {
     (TeamModel.findAll as sinon.SinonStub).restore();
   });
-
+  
   it('Tests if get all teams', async () => {
-    chaiHttpResponse = await chai.request(app).get('/teams')
+    
+    chaiHttpResponse = await chai.request(app).get('/teams');
+    
+    expect(chaiHttpResponse.status).to.be.equal(200);
+  });
 
+  
+
+  // it('Seu sub-teste', () => {
+  //   expect(false).to.be.eq(true);
+  // });
+});
+
+describe('Testing route /teams/:id', () => {
+  /**
+   * Exemplo do uso de stubs com tipos
+   */
+  const mockTeam = {id:5, teamName:"Cruzeiro"};
+  
+  let chaiHttpResponse: Response;
+
+  beforeEach(async () => {
+    sinon
+      .stub(TeamModel, "findOne")
+      .resolves(mockTeam as TeamModel);
+  });
+
+  afterEach(() => {
+    (TeamModel.findOne as sinon.SinonStub).restore();
+  });
+
+  it('Tests if get team by id', async () => {
+   
+
+    chaiHttpResponse = await chai.request(app).get('/teams/5')
     expect(chaiHttpResponse.status).to.be.equal(200)
+    expect(chaiHttpResponse.body).to.be.deep.equal({id:5, teamName:"Cruzeiro"})
+
   });
 
   // it('Seu sub-teste', () => {
   //   expect(false).to.be.eq(true);
   // });
 });
+
+
