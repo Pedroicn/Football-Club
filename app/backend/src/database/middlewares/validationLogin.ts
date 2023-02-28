8b2708c1e2675e8da656dff1ad0c555a6b1605c8
Pedro Nunes <pedro.icn88@gmail.com>
2023-02-28 18:26:39 -0300

    feat: create middleware for login validation

---
+++ app/backend/src/database/middlewares/validationLogin.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from 'express';
+import statusCodes from '../../utils/statusCodes';
+
+export const validationEmail = (req: Request, res: Response, next: NextFunction) => {
+  const { email } = req.body;
+  if (!email) {
+    res.status(statusCodes.badRequest).json({ message: 'All fields must be filled' });
+  }
+  next();
+};
+
+export const validationPassword = (req: Request, res: Response, next: NextFunction) => {
+  const { password } = req.body;
+  if (!password) {
+    res.status(statusCodes.badRequest).json({ message: 'All fields must be filled' });
+  }
+  next();
+};
--- app/backend/src/routes/loginRoutes.ts
+++ app/backend/src/routes/loginRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import UserController from '../database/controllers/userController';
+import { validationEmail, validationPassword } from '../database/middlewares/validationLogin';
 
 const router = Router();
 
 const userController = new UserController();
 
-router.post('/login', userController.user);
+router.post('/login', validationEmail, validationPassword, userController.user);
 
 export default router;
