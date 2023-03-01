96f725faf680fbc2c1351cbea431330d70a47e11
Pedro Nunes <pedro.icn88@gmail.com>
2023-03-01 14:02:34 -0300

    feat: create a validation middleware for the token

---
+++ app/backend/src/database/middlewares/validateToken.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from 'express';
+import * as jwt from 'jsonwebtoken';
+import statusCodes from '../../utils/statusCodes';
+
+const secretKey = process.env.JWT_SECRET || 'jwt_secret';
+
+const validateToken = (req: Request, res: Response, next: NextFunction) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(statusCodes.unauthorized).send({ message: 'Token not found' });
+  }
+
+  try {
+    const decoded = jwt.verify(authorization, secretKey);
+    res.locals.user = decoded;
+    next();
+  } catch (error) {
+    res.status(statusCodes.unauthorized).json({ message: 'Token must be a valid token' });
+  }
+  next();
+};
+
+export default validateToken;
--- app/backend/src/routes/loginRoutes.ts
+++ app/backend/src/routes/loginRoutes.ts
@@ -1,4 +1,6 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
+import statusCodes from '../utils/statusCodes';
+import validateToken from '../database/middlewares/validateToken';
 import UserController from '../database/controllers/userController';
 import { validationEmail, validationPassword } from '../database/middlewares/validationLogin';
 
@@ -7,5 +9,12 @@ const router = Router();
 const userController = new UserController();
 
 router.post('/login', validationEmail, validationPassword, userController.user);
+
+router.get(
+  '/login/role',
+  validateToken,
+  (_req: Request, res: Response) => res.status(statusCodes.ok)
+    .json({ role: res.locals.user.role }),
+);
 
 export default router;
