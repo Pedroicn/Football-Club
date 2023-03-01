f8ca0f345e95c03f5f557234df0ae127ed7a9f0b
Pedro Nunes <pedro.icn88@gmail.com>
2023-03-01 09:48:02 -0300

    feat: create TDD test for incorrect login params

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -4,6 +4,7 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
+import statusCodes from '../utils/statusCodes';
 
 import UserModel from '../database/models/UserModel';
 import { Response } from 'superagent';
@@ -68,5 +69,71 @@ describe('Testing route /login', () => {
     expect(chaiHttpResponse.status).to.be.equal(400);
     expect(chaiHttpResponse.body.message).to.be.equal('All fields muts be filled');
   });
+
+});
+
+describe('Testing route /login with incorrect params', () => {
+ 
+  const mockUser = {
+    id: 1,
+    username: "Pedro",
+    role: "user",
+    password: "secret_user",
+    email: "user@user.com",
+  }
+
+  const mockInvalidEmail = {
+    email: 'test.com', 
+    password:'secret_user'
+  };
+  
+  const mockInvalidPass = {
+    email: 'user@user.com', 
+    password:'123'
+  };
+  let chaiHttpResponse: Response;
+
+  beforeEach(async () => {
+    sinon
+      .stub(UserModel, "findOne")
+      .resolves(mockUser as UserModel);
+  });
+
+  afterEach(() => {
+    (UserModel.findOne as sinon.SinonStub).restore();
+  });
+
+  it('Tests login user with incorrect email', async () => {
+   
+    chaiHttpResponse = await chai
+      .request(app)
+      .post('/login').send(mockInvalidEmail)
+    expect(chaiHttpResponse.status).to.be.equal(statusCodes.unauthorized);
+    expect(chaiHttpResponse.body.message).to.be.equal('Invalid email or password');
+
+  });
+
+  it('Tests login user with incorrect password', async () => {
+   
+    chaiHttpResponse = await chai
+      .request(app)
+      .post('/login').send(mockInvalidPass)
+    expect(chaiHttpResponse.status).to.be.equal(statusCodes.unauthorized);
+    expect(chaiHttpResponse.body.message).to.be.equal('Invalid email or password');
+  });
+
+  it('Tests loginUser with email in the correct format but is not registered', async () => {
+  
+    chaiHttpResponse = await chai
+      .request(app)
+      .post('/login').send({
+        email: 'pedro@test.com',
+        password: 'secret_user',
+      })
+    expect(chaiHttpResponse.status).to.be.equal(statusCodes.unauthorized);
+    expect(chaiHttpResponse.body.message).to.be.equal('Invalid email or password');
+
+  });
+
 
 });
